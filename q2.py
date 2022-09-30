59ccef8a85d17f0b43a645017182ab9275862ada
Jack <jackcshin@gmail.com>
2022-09-30 15:25:29 -0700

    1st commit

--- q2.py
+++ q2.py
@@ -6,4 +6,4 @@ while True:
     for i in range(len(strings)-1,0,-1):
         if len(strings[i]) > len(strings[i-1]):
             strings[i],strings[i-1] = strings[i-1],strings[i]
-print(f"{strings[0]} {strings[len(strings)-2]}")
+print(f"{strings[0]} {strings[len(strings)-2]}") 
